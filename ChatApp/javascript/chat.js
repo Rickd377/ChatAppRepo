a62dea870e480633f7b715ce8469877098fa0e80
Rickd377 <rick-2007@outlook.com>
2024-03-12 16:56:10 +0100

    messages partly shown last send

---
+++ ChatApp/javascript/chat.js
@@ -0,0 +1,42 @@
+const form = document.querySelector(".typing-area"),
+inputField = form.querySelector(".input-field"),
+sendBtn = form.querySelector("button"),
+chatBox = document.querySelector(".chat-box");
+
+form.onsubmit = (e) => {
+    e.preventDefault();
+  };  
+
+sendBtn.onclick = () => {
+    // AJAX
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "php/insert-chat.php", true);
+    xhr.onload = () => {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+        if (xhr.status === 200) {
+          inputField.value = "";
+        }
+      }
+    };
+    let formData = new FormData(form);
+    xhr.send(formData);
+}
+
+setInterval(() => {
+    // let's start Ajax
+    let xhr = new XMLHttpRequest(); //creating XML object
+    xhr.open("POST", "php/get-chat.php", true);
+    xhr.onload = () => {
+        if(xhr.readyState === XMLHttpRequest.DONE){
+            if(xhr.status === 200){
+                let data = xhr.response;
+                chatBox.innerHTML = data;
+            }
+        }
+    }
+
+
+    let formData = new FormData(form);
+    xhr.send(formData);
+    xhr.send();
+}, 500); //this function will run frequently after 500ms
