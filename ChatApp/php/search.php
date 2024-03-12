5a2495083be2a4865aaae845aec9a4db82d42f29
Rickd377 <rick-2007@outlook.com>
2024-03-12 16:59:30 +0100

    search also fixed after message fix

--- ChatApp/php/search.php
+++ ChatApp/php/search.php
@@ -1,13 +1,15 @@
 <?php
-include_once "config.php";
-$searchTerm = mysqli_real_escape_string($conn, $_POST["searchTerm"]);
-$output = "";
-$sql = mysqli_query($conn, "SELECT * FROM users WHERE fname LIKE '%{$searchTerm}%' OR lname LIKE '%{$searchTerm}%'");
+session_start();
+    include_once "config.php";
+    $outgoing_id = $_SESSION['unique_id'];
+    $searchTerm = mysqli_real_escape_string($conn, $_POST["searchTerm"]);
+    $output = "";
+    $sql = mysqli_query($conn, "SELECT * FROM users WHERE fname LIKE '%{$searchTerm}%' OR lname LIKE '%{$searchTerm}%'");
 
-if(mysqli_num_rows($sql) > 0) {
-    include "data.php";
-}else{
-    $output .="No user found related to your search term";
-}
-echo $output;
+    if (mysqli_num_rows($sql) > 0) {
+        include "data.php";
+    } else {
+        $output .= "No user found related to your search term";
+    }
+    echo $output;
 ?>
